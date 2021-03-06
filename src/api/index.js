ddb8d7d85aac089b8cc0d28cf575434b6020838f
Jenil-Vekaria <jenil@jbbmobile.com>
2021-03-06 15:22:13 -0500

    Add Background Image fetch from Unsplash API

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,50 @@
 import axios from 'axios'
 
-const ADVICE_URL = 'https://api.adviceslip.com/advice';
-
-export const fetchAdvice = async () => {
+export const fetchQuote = async () => {
     try {
-        const response = axios.get(ADVICE_URL)
-        const { data: { slip: { advice } } } = await response
+        const storedQuote = localStorage.getItem('quote')
+        if (storedQuote) {
+            console.log('Getting Stored Quote')
+            return JSON.parse(storedQuote)
+        }
+        else {
+            const response = axios.get(process.env.REACT_APP_QUOTE_API_URL)
+            const { data } = await response
+            const { contents: { quotes } } = data
+            const { quote, author } = quotes[0]
+            console.log('Getting New Quote')
+            localStorage.setItem('quote', JSON.stringify({ quote, author }))
+            return { quote, author }
+        }
 
-        return advice
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error)
     }
 
-    return ''
+    return {}
+}
+
+export const fetchBackground = async () => {
+    try {
+        const backgroundImage = localStorage.getItem('backgroundImage')
+        if (backgroundImage) {
+            console.log('Getting Stored Background Url')
+            return backgroundImage
+        }
+        else {
+            const requestURL = `${process.env.REACT_APP_UNSPLASH_API_URL}client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&orientation=landscape&query=landscape`
+            const request = axios.get(requestURL)
+            const response = await request;
+            const { data: { urls: { full } } } = response
+
+            console.log('Getting New Background Url')
+            localStorage.setItem('backgroundImage', full)
+            return full
+        }
+    }
+    catch (error) {
+        console.error(error)
+    }
+
 }
