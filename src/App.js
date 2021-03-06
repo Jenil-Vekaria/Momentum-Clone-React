bba8d24c71dbb700dc44d24052981f187aedddb4
Jenil-Vekaria <jenil@jbbmobile.com>
2021-03-06 15:49:26 -0500

    Removed unsplash API and using Quest REST API background given in the response body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { fetchAdvice } from './api'
+import { fetchBackground, fetchQuote } from './api'
 import './App.css'
 
 export class App extends Component {
@@ -8,27 +8,45 @@ export class App extends Component {
     super(props)
 
     this.state = {
-      advice: ''
+      quote: '',
+      author: '',
+      backgroundImage: ''
     }
   }
 
   async componentDidMount() {
-    this.getAdvice()
+    this.getQuote()
   }
 
-  getAdvice = async () => {
-    const advice = await fetchAdvice()
-    this.setState({
-      advice: advice
-    })
+  getQuote = async () => {
+    const { quote, author, background } = await fetchQuote()
+
+    if (quote && author && background) {
+      this.setState({
+        quote,
+        author,
+        backgroundImage: background
+      })
+    }
   }
-
-
 
   render() {
+
+    const appStyle = {
+      backgroundImage: `linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)), url('${this.state.backgroundImage}')`
+    }
     return (
-      <div>
-        <h4>{this.state.advice}</h4>
+      <div className="App">
+        <div className="background" style={appStyle}></div>
+        <div className="welcomeContainer">
+          <p className="clock">12:00</p>
+          <p className="welcomeMessgage">Good Evening, Jenil</p>
+        </div>
+
+        <div className="quote-container">
+          <p className="quote">"{this.state.quote}"</p>
+          <p className="author">-{this.state.author}</p>
+        </div>
       </div>
     )
   }
--- src/api/index.js
+++ src/api/index.js
@@ -3,18 +3,18 @@ import axios from 'axios'
 export const fetchQuote = async () => {
     try {
         const storedQuote = localStorage.getItem('quote')
+
         if (storedQuote) {
-            console.log('Getting Stored Quote')
             return JSON.parse(storedQuote)
         }
         else {
             const response = axios.get(process.env.REACT_APP_QUOTE_API_URL)
             const { data } = await response
             const { contents: { quotes } } = data
-            const { quote, author } = quotes[0]
-            console.log('Getting New Quote')
-            localStorage.setItem('quote', JSON.stringify({ quote, author }))
-            return { quote, author }
+            const { quote, author, background } = quotes[0]
+
+            localStorage.setItem('quote', JSON.stringify({ quote, author, background }))
+            return { quote, author, background }
         }
 
     }
@@ -22,29 +22,5 @@ export const fetchQuote = async () => {
         console.error(error)
     }
 
-    return {}
-}
-
-export const fetchBackground = async () => {
-    try {
-        const backgroundImage = localStorage.getItem('backgroundImage')
-        if (backgroundImage) {
-            console.log('Getting Stored Background Url')
-            return backgroundImage
-        }
-        else {
-            const requestURL = `${process.env.REACT_APP_UNSPLASH_API_URL}client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&orientation=landscape&query=landscape`
-            const request = axios.get(requestURL)
-            const response = await request;
-            const { data: { urls: { full } } } = response
-
-            console.log('Getting New Background Url')
-            localStorage.setItem('backgroundImage', full)
-            return full
-        }
-    }
-    catch (error) {
-        console.error(error)
-    }
-
+    return ''
 }
